672ebf5ff08126699e3d9a7a95224de8fb3e1d64
Anne Maiale <anne.maiale@nuix.com>
2015-08-29 21:23:14 -0400

    filtering suggested tabs as you type

--- tagList/NewEntryViewController.m
+++ tagList/NewEntryViewController.m
@@ -11,18 +11,27 @@
 #import "TLtag.h"
 #import "CoreDataStack.h"
 
-@interface NewEntryViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface NewEntryViewController () <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
 
 @property (strong, nonatomic) IBOutlet UITextField *textField;
+@property (strong, nonatomic) IBOutlet UITableView* tableView;
+
 @property (strong, nonatomic) NSFetchedResultsController* existingTagsFetchedResultsController;
+@property (strong, nonatomic) NSMutableArray *matchingTagsArray;
+
+@property (nonatomic) NSRange tagRange;
+@property (nonatomic) BOOL isTypingTag;
+
 @end
 
 @implementation NewEntryViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [[self textField] setDelegate:self];
     [self.existingTagsFetchedResultsController performFetch:nil];
-    
+    [self createMatchingTagsArray];
+    [self createAutocompleteTableView];
 
     // Do any additional setup after loading the view.
 }
@@ -45,6 +54,93 @@
 -(void)dismissSelf {
     [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
+
+-(void)createMatchingTagsArray {
+//    NSPredicate *pre = [NSPredicate predicateWithFormat:@"attribute CONTAINS [cd] %@", searchString];
+//    NSArray *searchResults = [[self.fetchedResultsController fetchedObjects] filteredArrayUsingPredicate:pre]
+
+    
+    
+    NSArray* tagsList = [self.existingTagsFetchedResultsController fetchedObjects];
+    _matchingTagsArray = [[NSMutableArray alloc] initWithArray:tagsList];
+}
+
+-(void)createAutocompleteTableView {
+    UITableView* autocompleteTableView = _tableView;
+    autocompleteTableView.delegate = self;
+    autocompleteTableView.dataSource = self;
+    autocompleteTableView.scrollEnabled = YES;
+    autocompleteTableView.hidden = YES;
+    [self.view addSubview:autocompleteTableView];
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    
+    //Check if the current character (string) is #. If so, set _tagBeginRange to [range.location, 0]; increment the
+    // range.length as we continue typing.
+
+    if ([string isEqualToString:@"#"]) {
+        _isTypingTag = YES;
+        _tableView.hidden = NO;
+        _tagRange = NSMakeRange (range.location, 0);
+
+    } else if (_isTypingTag) {
+        
+        _tagRange.length = range.location - _tagRange.location;
+        NSString *substring = [textField.text substringWithRange:_tagRange];
+//        NSString *substring = [NSString stringWithString:textField.text];
+//        substring = [substring
+//                     stringByReplacingCharactersInRange:_tagBeginRange withString:string];
+        
+        [self searchAutocompleteEntriesWithSubstring:substring];
+    }
+    
+    return YES;
+}
+
+- (void)searchAutocompleteEntriesWithSubstring:(NSString *)substring {
+    NSMutableArray *autocompleteTags = [[NSMutableArray alloc] init];
+    
+    // Put anything that contains with this substring into the autocompleteUrls array
+    // The items in this array is what will show up in the table view
+    for(TLtag *curTag in _matchingTagsArray) {
+        NSString *curString = [curTag valueForKey:@"text"];
+        NSRange substringRange = [curString rangeOfString:substring];
+        if (substringRange.location != 0) {
+            [autocompleteTags removeObject:curTag];
+        }
+    }
+    _matchingTagsArray = autocompleteTags;
+    [_tableView reloadData];
+}
+
+// Autocompleting search table
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"Cell"];
+    }
+    // Configure the cell...
+    TLtag *tag = [self.matchingTagsArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = tag.text;
+    
+    return cell;
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _existingTagsFetchedResultsController.fetchedObjects.count;
+};
+
+
+
+
 
 
 // Parsing text and adding item & tags.
